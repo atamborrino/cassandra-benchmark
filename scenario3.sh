e371c4271b241823b6c574b76c5ae9b7296e4df1
Noura Rabardeau <nrabardeau@gmail.com>
2013-04-29 16:23:31 +0200

    scenario 2&3

--- scenario3.sh
+++ scenario3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create a new instance with 4 nodes
-ccm create scenarioTwo -v 1.2.0
+ccm create scenarioThree -v 1.2.0
 ccm populate -n 8
 ccm start
 export CASS_HOST=127.0.0.1
@@ -19,31 +19,31 @@ echo -e '=== Cassandra Stress Program found ===\n'
 
 # Launch the cassandra-stress
 echo -e '=== Beginning to insert 10000 rows ===\n'
-tools/bin/cassandra-stress -d $CASS_HOST -l 3 -n 10000 -o insert #-e ONE
+tools/bin/cassandra-stress -d $CASS_HOST -l 6 -n 10000 -o insert #-e ONE
 echo -e '=== Insert 10000 OK === \n'
 echo -e '\n === Beginning to read 10000 rows === '
-tools/bin/cassandra-stress -d $CASS_HOST -l 3 -n 10000 -o read #-e ALL
+tools/bin/cassandra-stress -d $CASS_HOST -l 6 -n 10000 -o read #-e ALL
 echo -e '=== Read 10000 rows OK === \n'
 
 # Rendering the analysis
 echo -e '\n === Rendering the analysis - 50 MS === \n'
-bin/nodetool -h $CASS_HOST -p 7100 predictconsistency 3 50 1
+bin/nodetool -h $CASS_HOST -p 7100 predictconsistency 6 50 1
 
 echo -e '\n === Rendering the analysis - 100 MS=== \n'
-bin/nodetool -h $CASS_HOST -p 7100 predictconsistency 3 100 1
+bin/nodetool -h $CASS_HOST -p 7100 predictconsistency 6 100 1
 
 echo -e '\n === Rendering the analysis - 200 MS=== \n'
-bin/nodetool -h $CASS_HOST -p 7100 predictconsistency 3 200 1
+bin/nodetool -h $CASS_HOST -p 7100 predictconsistency 6 200 1
 
 echo -e '\n === Rendering the analysis - 500 MS=== \n'
-bin/nodetool -h $CASS_HOST -p 7100 predictconsistency 3 500 1
+bin/nodetool -h $CASS_HOST -p 7100 predictconsistency 6 500 1
 
 echo -e '\n === Rendering the analysis - 1000 MS=== \n'
-bin/nodetool -h $CASS_HOST -p 7100 predictconsistency 3 1000 1
+bin/nodetool -h $CASS_HOST -p 7100 predictconsistency 6 1000 1
 
 echo -e "=== C'est fini les cocos ! === \n\n"
 
 # Cleaning all our stuffs
 ccm stop
-ccm remove scenarioTwo
+ccm remove scenarioThree
 echo -e '=== Clusters removed === \n'
